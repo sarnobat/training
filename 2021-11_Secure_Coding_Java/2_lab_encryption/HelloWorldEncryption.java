76d4a7337f531380642a56e47a9737436b0c8e21
Sridhar Sarnobat.deprecated <sarnobat@Sridhars-MacBook-Air.local>
2021-11-19 17:45:00 -0800

    Minor changes (untested)

--- 2021-11_Secure_Coding_Java/2_lab_encryption/HelloWorldEncryption.java
+++ 2021-11_Secure_Coding_Java/2_lab_encryption/HelloWorldEncryption.java
@@ -5,18 +5,18 @@ pubic class HelloWorldEncryption {
 
   public static void main(String[] args) {
 
-    String msgText = "This is a message created by Komal";
-eyvar keyGen = KeyGenerator.getInstance("
-  AES"); keyGen.init(new SecureRandom());
-    var key = keyGen.generateKey();
-e[] byteypencrtypte encrypt(msgTex);
+		String msgText = "This is a message created by Komal";
+		KeyGenerator keyGen = KeyGenerator.getInstance("AES"); 
+		keyGen.init(new SecureRandom());
+		SecureKey key = keyGen.generateKey();
+		byte[] encrtypted = encrypt(msgTex);
 
-  }
+	}
 
-  byte[] encrypt(String msg, Key key, Cipher cipher) throws Exception { 
+	byte[] encrypt(String msg, Key key, Cipher cipher) throws Exception { 
 
-    cipher.init(Cipher.ENCRYPT_MODE, key);
-    byte[] input = msg.getBytes();
-    return cipher.doFinal(input); 
-  }
+		cipher.init(Cipher.ENCRYPT_MODE, key);
+		byte[] input = msg.getBytes();
+		return cipher.doFinal(input); 
+	}
 }
