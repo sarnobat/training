949bd6b4b451d0334965ff03f93e28a15a1f747e
Sridhar Sarnobat.deprecated <sarnobat@Sridhars-MacBook-Air.local>
2021-11-19 17:54:34 -0800

    Minor changes (untested)

--- 2021-11_Secure_Coding_Java/2_lab_encryption/HelloWorldEncryption.java
+++ 2021-11_Secure_Coding_Java/2_lab_encryption/HelloWorldEncryption.java
@@ -1,22 +1,37 @@
+import java.security.*;
+import javax.crypto.*;
+
 /**
  * Komal, Sridhar lab slide ~115
  */
-pubic class HelloWorldEncryption {
+public class HelloWorldEncryption {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws Exception {
 
 		String msgText = "This is a message created by Komal";
 		KeyGenerator keyGen = KeyGenerator.getInstance("AES"); 
 		keyGen.init(new SecureRandom());
-		SecureKey key = keyGen.generateKey();
-		byte[] encrtypted = encrypt(msgTex);
+		Key key = keyGen.generateKey();
+		byte[] encrypted = encrypt(msgText, key, Cipher.getInstance("AES/ECB/PKCS5Padding"));
 
+		System.out.println("encrypted text: " + new String(encrypted));
+		
+		byte[] decrypted = decrypt(encrypted, key, Cipher.getInstance("AES/ECB/PKCS5Padding"));
+		
+		System.out.println("decrypted text: " + new String(decrypted));
 	}
 
-	byte[] encrypt(String msg, Key key, Cipher cipher) throws Exception { 
+	private static byte[] encrypt(String msg, Key key, Cipher cipher) throws Exception { 
 
 		cipher.init(Cipher.ENCRYPT_MODE, key);
 		byte[] input = msg.getBytes();
 		return cipher.doFinal(input); 
+	}
+	
+	
+	static byte[] decrypt(byte[] cipherText, Key key, Cipher cipher) throws Exception {
+
+		cipher.init(Cipher.DECRYPT_MODE, key);
+		return cipher.doFinal(cipherText); 
 	}
 }
---
+++ 2021-11_Secure_Coding_Java/2_lab_encryption/run.sh
@@ -0,0 +1,3 @@
+unset JAVA_HOME
+JAVA_HOME=/usr/local/Cellar/openjdk@11/11.0.12/
+/usr/local/Cellar/openjdk@11/11.0.12/bin/java HelloWorldEncryption.java
